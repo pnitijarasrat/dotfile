068ce030983a70a5c4730b132fa1b575915bf3dc
pnitijarasrat <puriwatds@gmail.com>
2025-01-03 19:06:29 +0700

    config org mode

--- nvim/lua/plugins/org.lua
+++ nvim/lua/plugins/org.lua
@@ -6,19 +6,18 @@ return {
     require("orgmode").setup({
       org_agenda_files = "~/personal/org/**/*",
       org_default_notes_file = "~/personal/refile.org",
-      win_split_mode = "vsplit",
+      win_split_mode = "tabnew",
       org_todo_keywords = { "TODO(t)", "MEETING(m)", "APPOINT(a)", "WORKOUT(w)", "|", "DONE(d)" },
       org_deadline_warning_days = 7,
       org_startup_folded = "content",
       org_hide_leading_stars = true,
     })
-    vim.api.nvim_set_hl(0, "@org.agenda.scheduled", { fg = "#EDEFF3" })
+    vim.api.nvim_set_hl(0, "@org.agenda.scheduled", { link = "SignColumn" })
     vim.api.nvim_set_hl(0, "@org.agenda.scheduled_past", { link = "ERROR" })
     vim.api.nvim_set_hl(0, "@org.agenda.deadline", { link = "ERROR" })
-    vim.api.nvim_set_hl(0, "@org.agenda.today", { link = "TermCursor" })
-    vim.api.nvim_set_hl(0, "@org.keyword.todo", { link = "DiffDelete" })
-    vim.api.nvim_set_hl(0, "@org.keyword.done", { link = "DiffAdd" })
-    vim.api.nvim_set_hl(0, "@org.headline.level1", { link = "CurSearch" })
-    vim.api.nvim_set_hl(0, "@org.headline.level2", { fg = "#D8DEE9" })
+    vim.api.nvim_set_hl(0, "@org.agenda.today", { link = "Search" })
+    vim.api.nvim_set_hl(0, "@org.agenda.day", { link = "MoreMsg" })
+    vim.api.nvim_set_hl(0, "@org.keyword.todo", { link = "DiagnosticError" })
+    vim.api.nvim_set_hl(0, "@org.keyword.done", { link = "DiagnosticHint" })
   end,
 }
