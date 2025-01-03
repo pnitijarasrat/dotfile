b52a95821d0b798ec22d94757ebf16e05180e82f
pnitijarasrat <puriwatds@gmail.com>
2025-01-03 21:40:44 +0700

    config lua line

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,59 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.g.lualine_laststatus = vim.o.laststatus
+    if vim.fn.argc(-1) > 0 then
+      vim.o.statusline = " "
+    else
+      vim.o.laststatus = 0
+    end
+  end,
+  opts = function()
+    -- PERF: we don't need this lualine require madness 🤷
+    local lualine_require = require("lualine_require")
+    lualine_require.require = require
+
+    vim.o.laststatus = vim.g.lualine_laststatus
+
+    local opts = {
+      options = {
+        theme = "auto",
+        globalstatus = vim.o.laststatus == 3,
+        disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "filename" },
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = { "branch" },
+        lualine_z = {
+          "searchcount",
+        },
+      },
+      extensions = { "neo-tree", "lazy", "fzf" },
+    }
+    if vim.g.trouble_lualine and LazyVim.has("trouble.nvim") then
+      local trouble = require("trouble")
+      local symbols = trouble.statusline({
+        mode = "symbols",
+        groups = {},
+        title = false,
+        filter = { range = true },
+        format = "{kind_icon}{symbol.name:Normal}",
+        hl_group = "lualine_c_normal",
+      })
+      table.insert(opts.sections.lualine_c, {
+        symbols and symbols.get,
+        cond = function()
+          return vim.b.trouble_lualine ~= false and symbols.has()
+        end,
+      })
+    end
+
+    return opts
+  end,
+}
