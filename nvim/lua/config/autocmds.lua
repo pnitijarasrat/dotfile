f8cae1bc094040d61022a2301c90696cf1039941
pnitijarasrat <puriwatds@gmail.com>
2025-01-04 21:40:06 +0700

    turn off autoformat

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,13 +1,10 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
-vim.api.nvim_create_autocmd("BufEnter", {
-  callback = function()
-    vim.opt.fillchars:append({ eob = "~" })
-  end,
-})
+-- add ~ on blank lines
+vim.opt.fillchars:append({ eob = "~" })
+
+-- format myself
+vim.g.autoformat = false
+vim.opt.tabstop = 4        -- Number of spaces a <Tab> counts for
+vim.opt.shiftwidth = 4     -- Number of spaces for indentation
+vim.opt.expandtab = true   -- Use spaces instead of tabs
+vim.opt.autoindent = true  -- Copy indentation from the previous line
+vim.opt.smartindent = true -- Automatically insert extra indentation in some cases
