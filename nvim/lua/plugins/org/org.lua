223652da59e8c65456963725f125723acd67f074
pnitijarasrat <puriwatds@gmail.com>
2025-01-07 20:05:09 +0700

    add more keyword for todo and done, also change day color

--- nvim/lua/plugins/org/org.lua
+++ nvim/lua/plugins/org/org.lua
@@ -7,7 +7,7 @@ return {
             org_agenda_files = "~/personal/org/**/*",
             org_default_notes_file = "~/personal/refile.org",
             win_split_mode = "tabnew",
-            org_todo_keywords = { "NEXT(n)","TODO(t)", "MEETING(m)", "APPOINT(a)", "WORKOUT(w)", "|", "DONE(d)", "CANCELLED(c)", "SKIPPED(s)" },
+            org_todo_keywords = { "NEXT(n)", "TODO(t)", "MEETING(m)", "APPOINT(a)", "WORKOUT(w)", "|", "DONE(d)", "CANCELLED(c)", "SKIPPED(s)" },
             org_deadline_warning_days = 7,
             org_startup_folded = "showeverything",
             org_hide_leading_stars = true,
@@ -18,9 +18,15 @@ return {
                     org_toggle_checkbox = "<C-CR>",
                 },
             },
+            org_todo_keyword_faces = {
+                MEETING = ':foreground #d3869d',
+                APPOINT = ':foreground #a9b665',
+                SKIPPED = ':foreground #472322'
+            }
         })
         vim.api.nvim_set_hl(0, "@org.agenda.scheduled", { link = "TermCursorNc" })
-        vim.api.nvim_set_hl(0, "@org.agenda.today", { link = "Underlined" })
+        vim.api.nvim_set_hl(0, "@org.agenda.today", { link = "TSURI" })
+        vim.api.nvim_set_hl(0, "@org.agenda.day", { link = "InfoFloat" })
         vim.api.nvim_set_hl(0, "@org.keyword.todo", { link = "DiagnosticOk" })
         vim.api.nvim_set_hl(0, "@org.keyword.done", { link = "WhiteSpace" })
 
